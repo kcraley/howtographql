1fe0d352f48dca1a916b82cb2031221d764b18c6
Keith Craley <kcral21@gmail.com>
2022-05-18 00:23:35 -0400

    update create link logic to store in db

--- graph/schema.resolvers.go
+++ graph/schema.resolvers.go
@@ -6,19 +6,23 @@ package graph
 import (
 	"context"
 	"fmt"
+	"strconv"
 
 	"github.com/kcraley/howtographql/graph/generated"
 	"github.com/kcraley/howtographql/graph/model"
+	"github.com/kcraley/howtographql/internal/links"
 )
 
 func (r *mutationResolver) CreateLink(ctx context.Context, input model.NewLink) (*model.Link, error) {
-	var link model.Link
-	var user model.User
-	link.Address = input.Address
+	var link links.Link
 	link.Title = input.Title
-	user.Name = "test"
-	link.User = &user
-	return &link, nil
+	link.Address = input.Address
+	linkID := link.Save()
+	return &model.Link{
+		ID:      strconv.FormatInt(linkID, 10),
+		Title:   link.Title,
+		Address: link.Address,
+	}, nil
 }
 
 func (r *mutationResolver) CreateUser(ctx context.Context, input model.NewUser) (string, error) {
